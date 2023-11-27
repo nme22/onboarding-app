b1b27b1b3869a64f73f8ba18933c7c7def21b461
Navid Ebrahimi <navidebrahimi@macbook-pro.mynetworksettings.com>
2023-11-27 12:16:38 -0500

    adding some extra messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div className='bg-blue-500'>
       <UserButton afterSignOutUrl='/' />
+
+      <p>Adding a note that I'm very excited about this new job!</p>
+      <p>Adding another note that everyone on the team is awesome!</p>
     </div>
   );
 }
